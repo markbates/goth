cb693690474d3b5d2b6a6d2be7e2cae568753e6b
Craig P Jolicoeur <cpjolicoeur@gmail.com>
2015-01-28 13:22:59 -0500

    Migrate gplus provider to oauth2 package

--- providers/gplus/gplus_test.go
+++ providers/gplus/gplus_test.go
@@ -1,10 +1,12 @@
-package gplus
+package gplus_test
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
 	"github.com/markbates/goth"
+	"github.com/markbates/goth/providers/gplus"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -16,6 +18,20 @@ func Test_New(t *testing.T) {
 	a.Equal(provider.ClientKey, os.Getenv("GPLUS_KEY"))
 	a.Equal(provider.Secret, os.Getenv("GPLUS_SECRET"))
 	a.Equal(provider.CallbackURL, "/foo")
+}
+
+func Test_BeginAuth(t *testing.T) {
+	t.Parallel()
+	a := assert.New(t)
+
+	provider := gplusProvider()
+	session, err := provider.BeginAuth("test_state")
+	s := session.(*gplus.Session)
+	a.NoError(err)
+	a.Contains(s.AuthURL, "accounts.google.com/o/oauth2/auth")
+	a.Contains(s.AuthURL, fmt.Sprintf("client_id=%s", os.Getenv("GPLUS_KEY")))
+	a.Contains(s.AuthURL, "state=test_state")
+	a.Contains(s.AuthURL, "scope=profile+email+openid")
 }
 
 func Test_Implements_Provider(t *testing.T) {
@@ -33,11 +49,11 @@ func Test_SessionFromJSON(t *testing.T) {
 
 	s, err := provider.UnmarshalSession(`{"AuthURL":"https://accounts.google.com/o/oauth2/auth","AccessToken":"1234567890"}`)
 	a.NoError(err)
-	session := s.(*Session)
+	session := s.(*gplus.Session)
 	a.Equal(session.AuthURL, "https://accounts.google.com/o/oauth2/auth")
 	a.Equal(session.AccessToken, "1234567890")
 }
 
-func gplusProvider() *Provider {
-	return New(os.Getenv("GPLUS_KEY"), os.Getenv("GPLUS_SECRET"), "/foo")
+func gplusProvider() *gplus.Provider {
+	return gplus.New(os.Getenv("GPLUS_KEY"), os.Getenv("GPLUS_SECRET"), "/foo")
 }
