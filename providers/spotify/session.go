3b0d8b313d21cbb3908d121b1b22facf0a04d2ea
Zac Bergquist <zbergquist99@gmail.com>
2015-01-30 21:24:11 -0500

    Add provider for Spotify

---
+++ providers/spotify/session.go
@@ -0,0 +1,47 @@
+package spotify
+
+import (
+	"encoding/json"
+	"errors"
+
+	"github.com/markbates/goth"
+	"golang.org/x/oauth2"
+)
+
+// Session stores data during the auth process with Spotify.
+type Session struct {
+	AuthURL     string
+	AccessToken string
+}
+
+// GetAuthURL will return the URL set by calling the `BeginAuth` function on the
+// Spotify provider.
+func (s Session) GetAuthURL() (string, error) {
+	if s.AuthURL == "" {
+		return "", errors.New("spotify: AuthURL has not been set")
+	}
+	return s.AuthURL, nil
+}
+
+// Authorize completes the the authorization with Spotify and returns the access
+// token to be stored for future use.
+func (s Session) Authorize(provider goth.Provider, params goth.Params) (string, error) {
+	p := provider.(*Provider)
+	token, err := p.config.Exchange(oauth2.NoContext, params.Get("code"))
+	if err != nil {
+		return "", err
+	}
+	s.AccessToken = token.AccessToken
+	return token.AccessToken, err
+}
+
+// Marshal marshals a session into a JSON string.
+func (s Session) Marshal() string {
+	j, _ := json.Marshal(s)
+	return string(j)
+}
+
+// String is equivalent to Marshal.  It returns a JSON representation of the session.
+func (s Session) String() string {
+	return s.Marshal()
+}
---
+++ providers/spotify/spotify_test.go
@@ -0,0 +1,53 @@
+package spotify
+
+import (
+	"os"
+	"testing"
+
+	"github.com/markbates/goth"
+	"github.com/stretchr/testify/assert"
+)
+
+func provider() *Provider {
+	return New(os.Getenv("SPOTIFY_KEY"), os.Getenv("SPOTIFY_SECRET"), "/foo", "user")
+}
+
+func Test_New(t *testing.T) {
+	t.Parallel()
+	a := assert.New(t)
+	p := provider()
+
+	a.Equal(p.ClientKey, os.Getenv("SPOTIFY_KEY"))
+	a.Equal(p.Secret, os.Getenv("SPOTIFY_SECRET"))
+	a.Equal(p.CallbackURL, "/foo")
+}
+
+func Test_ImplementsProvider(t *testing.T) {
+	t.Parallel()
+	a := assert.New(t)
+	a.Implements((*goth.Provider)(nil), provider())
+}
+
+func Test_BeginAuth(t *testing.T) {
+	t.Parallel()
+	a := assert.New(t)
+
+	p := provider()
+	session, err := p.BeginAuth("test_state")
+	s := session.(*Session)
+	a.NoError(err)
+	a.Contains(s.AuthURL, "accounts.spotify.com/authorize")
+}
+
+func Test_SessionFromJSON(t *testing.T) {
+	t.Parallel()
+	a := assert.New(t)
+
+	p := provider()
+	session, err := p.UnmarshalSession(`{"AuthURL":"http://accounts.spotify.com/authorize","AccessToken":"1234567890"}`)
+	a.NoError(err)
+
+	s := session.(*Session)
+	a.Equal(s.AuthURL, "http://accounts.spotify.com/authorize")
+	a.Equal(s.AccessToken, "1234567890")
+}
