954214c1847012334377cd5770a27787f9de8cea
Mark Bates <mark@markbates.com>
2015-09-30 13:09:32 -0400

    regiser goth.User with the gob package

--- user.go
+++ user.go
@@ -1,5 +1,11 @@
 package goth
 
+import "encoding/gob"
+
+func init() {
+	gob.Register(User{})
+}
+
 // User contains the information common amongst most OAuth and OAuth2 providers.
 // All of the "raw" datafrom the provider can be found in the `RawData` field.
 type User struct {
