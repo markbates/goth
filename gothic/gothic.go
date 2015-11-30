592600a5c7db220406d832da4ad6093b60a73f11
smagic39 <smagic39@gmail.com>
2015-11-30 18:24:12 +0700

    update provider

--- gothic/gothic.go
+++ gothic/gothic.go
@@ -14,7 +14,7 @@ import (
 	"os"
 	"github.com/julienschmidt/httprouter"
 	"github.com/gorilla/sessions"
-	"github.com/markbates/goth"
+	"github.com/smagic39/goth"
 )
 
 // SessionName is the key used to access the session store.
