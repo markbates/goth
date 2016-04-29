567cb575c2af82e031c1d2e95a5a16aa285a03be
Geoff Franks <geoff.franks@gmail.com>
2016-04-29 14:43:24 -0400

    Added a 'cloudfoundry' OAuth2 provider.
    It works much the same as the other OAuth2 providers. However, since
    Cloud Foundry isn't typically a hosted service, the UAA base URL
    is configurable, to match that of your Cloud Foundry installation's
    UAA.

---
+++ providers/cloudfoundry/session.go
@@ -0,0 +1,67 @@
+package cloudfoundry
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/markbates/goth"
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
+	"strings"
+	"time"
+)
+
+// Session stores data during the auth process with Box.
+type Session struct {
+	AuthURL      string
+	AccessToken  string
+	RefreshToken string
+	ExpiresAt    time.Time
+}
+
+var _ goth.Session = &Session{}
+
+// GetAuthURL will return the URL set by calling the `BeginAuth` function on the Box provider.
+func (s Session) GetAuthURL() (string, error) {
+	if s.AuthURL == "" {
+		return "", errors.New("an AuthURL has not be set")
+	}
+	return s.AuthURL, nil
+}
+
+// Authorize the session with Cloud Foundry and return the access token to be stored for future use.
+func (s *Session) Authorize(provider goth.Provider, params goth.Params) (string, error) {
+	p := provider.(*Provider)
+	ctx := context.WithValue(oauth2.NoContext, oauth2.HTTPClient, p.Client)
+	token, err := p.config.Exchange(ctx, params.Get("code"))
+	if err != nil {
+		return "", err
+	}
+
+	if !token.Valid() {
+		return "", errors.New("Invalid token received from provider")
+	}
+
+	s.AccessToken = token.AccessToken
+	s.RefreshToken = token.RefreshToken
+	s.ExpiresAt = token.Expiry
+	fmt.Printf("TOKEN: %s\n", s.AccessToken)
+	return token.AccessToken, err
+}
+
+// Marshal the session into a string
+func (s Session) Marshal() string {
+	b, _ := json.Marshal(s)
+	return string(b)
+}
+
+func (s Session) String() string {
+	return s.Marshal()
+}
+
+// UnmarshalSession wil unmarshal a JSON string into a session.
+func (p *Provider) UnmarshalSession(data string) (goth.Session, error) {
+	s := &Session{}
+	err := json.NewDecoder(strings.NewReader(data)).Decode(s)
+	return s, err
+}
