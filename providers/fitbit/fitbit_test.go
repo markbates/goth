816910731248fb0499460f816c5962f9774e386f
Corey McGrillis <corey.mcgrillis@flybits.com>
2016-08-09 13:24:53 -0400

    changed dependency back to origin

--- providers/fitbit/fitbit_test.go
+++ providers/fitbit/fitbit_test.go
@@ -4,8 +4,8 @@ import (
 	"os"
 	"testing"
 
-	"github.com/coreytm/goth/providers/fitbit"
 	"github.com/markbates/goth"
+	"github.com/markbates/goth/providers/fitbit"
 	"github.com/stretchr/testify/assert"
 )
 
