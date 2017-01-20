0dc0ba5cbbc6f9d2827b58d8879cbf3ace3b34d2
willemvd <willem@opencirclesolutions.nl>
2017-01-20 12:26:23 +0100

    restore broken import

--- providers/auth0/auth0_test.go
+++ providers/auth0/auth0_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/markbates/goth"
 	"github.com/markbates/goth/providers/auth0"
 	"github.com/stretchr/testify/assert"
+	"gopkg.in/jarcoal/httpmock.v1"
 )
 
 func Test_New(t *testing.T) {
