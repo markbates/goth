97bd398a7cdd7168f993d414c2830bd10319682a
Richard Burk Orofeo <chardyy.orofeo@gmail.com>
2019-05-02 13:23:48 +0800

    Added line provider dir
    Signed-off-by: Richard Burk Orofeo <chardyy.orofeo@gmail.com>

---
+++ providers/line/line.go
@@ -0,0 +1,3 @@
+// Package line implements the OAuth2 protocol for authenticating users through line.
+// This package can be used as a reference implementation of an OAuth2 provider for Goth.
+package line
