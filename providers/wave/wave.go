84b20136a7f8ee44cf656566f5e91673c39c1aa0
Noah Tatko <tatkon@rbtrainers.com>
2019-10-28 16:47:51 -0500

    this won't work

--- providers/wave/wave.go
+++ providers/wave/wave.go
@@ -3,7 +3,13 @@
 package wave
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"net/url"
+	"strings"
 
 	"github.com/markbates/goth"
 	"golang.org/x/oauth2"
@@ -63,6 +69,7 @@ func (p *Provider) SetName(name string) {
 	p.providerName = name
 }
 
+// Client is to do some stuff
 func (p *Provider) Client() *http.Client {
 	return goth.HTTPClientWithFallBack(p.HTTPClient)
 }
@@ -77,4 +84,65 @@ func (p *Provider) BeginAuth(state string) (goth.Session, error) {
 		AuthURL: url,
 	}
 	return session, nil
+}
+
+type waveQuery struct {
+	Query string
+}
+
+// FetchUser will go to Wave and access basic information about the user.
+func (p *Provider) FetchUser(session goth.Session) (goth.User, error) {
+	sess := session.(*Session)
+	user := goth.User{
+		AccessToken: sess.AccessToken,
+		Provider:    p.Name(),
+	}
+
+	if user.AccessToken == "" {
+		// data is not yet retrieved since accessToken is still empty
+		return user, fmt.Errorf("%s cannot get user information without accessToken", p.providerName)
+	}
+
+	const aquery = `{ query: "query { name { id userinfo } }" }`
+	const query = waveQuery{}
+	json.Unmarshal([]byte(aquery, &query))
+	fmt.Println(query)
+
+	response, err := p.Client().Get(p.profileURL + "?access_token=" + url.QueryEscape(sess.AccessToken))
+	if err != nil {
+		return user, err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return user, fmt.Errorf("Wave API responded with a %d trying to fetch user information", response.StatusCode)
+	}
+
+	bits, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return user, err
+	}
+
+	err = json.NewDecoder(bytes.NewReader(bits)).Decode(&user.RawData)
+	if err != nil {
+		return user, err
+	}
+
+	err = userFromReader(bytes.NewReader(bits), &user)
+	if err != nil {
+		return user, err
+	}
+
+	if user.Email == "" {
+		for _, scope := range p.config.Scopes {
+			if strings.TrimSpace(scope) == "user" || strings.TrimSpace(scope) == "user:email" {
+				user.Email, err = getPrivateMail(p, sess)
+				if err != nil {
+					return user, err
+				}
+				break
+			}
+		}
+	}
+	return user, err
 }
