7cc1735ea051061fe967ce9f368c75285783d05c
David Stapleton <dstapleton@shipt.com>
2020-01-03 12:08:48 -0500

    Add email and is_private_email to Apple id_token

--- providers/apple/session_test.go
+++ providers/apple/session_test.go
@@ -35,7 +35,7 @@ func Test_ToJSON(t *testing.T) {
 	s := &Session{}
 
 	data := s.Marshal()
-	a.Equal(data, `{"AuthURL":"","AccessToken":"","RefreshToken":"","ExpiresAt":"0001-01-01T00:00:00Z","sub":""}`)
+	a.Equal(data, `{"AuthURL":"","AccessToken":"","RefreshToken":"","ExpiresAt":"0001-01-01T00:00:00Z","sub":"","email":"","is_private_email":false}`)
 }
 
 func Test_String(t *testing.T) {
