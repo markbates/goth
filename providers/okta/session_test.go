96e4ec7528fbf801d3e95a49eab0e101a8472051
toby <32752087+miticotoby@users.noreply.github.com>
2020-05-28 18:30:50 +0200

    Add Okta provider

---
+++ providers/okta/session_test.go
@@ -0,0 +1,47 @@
+package okta_test
+
+import (
+	"test/providers/okta"
+	"github.com/markbates/goth"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_Implements_Session(t *testing.T) {
+	t.Parallel()
+	a := assert.New(t)
+	s := &okta.Session{}
+
+	a.Implements((*goth.Session)(nil), s)
+}
+
+func Test_GetAuthURL(t *testing.T) {
+	t.Parallel()
+	a := assert.New(t)
+	s := &okta.Session{}
+
+	_, err := s.GetAuthURL()
+	a.Error(err)
+
+	s.AuthURL = "/foo"
+
+	url, _ := s.GetAuthURL()
+	a.Equal(url, "/foo")
+}
+
+func Test_ToJSON(t *testing.T) {
+	t.Parallel()
+	a := assert.New(t)
+	s := &okta.Session{}
+
+	data := s.Marshal()
+	a.Equal(data, `{"AuthURL":"","AccessToken":"","RefreshToken":"","ExpiresAt":"0001-01-01T00:00:00Z","UserID":""}`)
+}
+
+func Test_String(t *testing.T) {
+	t.Parallel()
+	a := assert.New(t)
+	s := &okta.Session{}
+
+	a.Equal(s.String(), s.Marshal())
+}
