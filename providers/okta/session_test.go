cbc24f44a192e4e40d6cfcdabde6317d85b49fdd
Ali Qornan <aliqornan@ymail.com>
2020-10-07 08:09:54 +0700

    Fix code formatting using gofmt

--- providers/okta/session_test.go
+++ providers/okta/session_test.go
@@ -1,8 +1,8 @@
 package okta_test
 
 import (
-	"github.com/markbates/goth/providers/okta"
 	"github.com/markbates/goth"
+	"github.com/markbates/goth/providers/okta"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
